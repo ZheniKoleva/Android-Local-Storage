2c247afa7e8ea2c57119a864663dc8b0d0456c73
ZheniKoleva <zhenie_koleva@abv.bg>
2022-11-07 18:13:05 +0200

    retrofit configuration, fragments, nav_graph added

---
+++ app/src/main/java/com/example/android_local_storage/CountriesListFragment.kt
@@ -0,0 +1,45 @@
+package com.example.android_local_storage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android_local_storage.databinding.FragmentCountriesListBinding
+
+class CountriesListFragment : Fragment() {
+    private lateinit var binding: FragmentCountriesListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCountriesListBinding.inflate(inflater, container, false)
+
+        /*val retrofit = Retrofit.Builder()
+            .baseUrl("https://restcountries.com/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val countryService = retrofit.create(CountryService::class.java)
+        val countryRepository = CountryRepository(countryService)  */
+        /*val countryRepository = CountryRepository()
+
+        countryRepository.getCountries()?.enqueue(object : Callback<List<Country>> {
+            override fun onResponse(
+                call: Call<List<Country>>, response: Response<List<Country>>
+            ) {
+                val countries = response.body() ?: return
+                val adapter = CountryAdapter(countries)
+                binding.countriesList.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<Country>>, t: Throwable) {
+                Snackbar.make(binding.root, "Failure to load countries", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }) */
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/android_local_storage/MainActivity.kt
+++ app/src/main/java/com/example/android_local_storage/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.android_local_storage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_local_storage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
